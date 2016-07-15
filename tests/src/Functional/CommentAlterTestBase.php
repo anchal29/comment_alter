554f521c76124b346f2cf225bb1decf3ac94240b
anchal29 <pandey.anchal.95@gmail.com>
2016-07-15 22:31:12 +0530

    Implemented hook_entity_revision_delete() and completed addField() function.

--- tests/src/Functional/CommentAlterTestBase.php
+++ tests/src/Functional/CommentAlterTestBase.php
@@ -71,22 +71,42 @@ class CommentAlterTestBase extends BrowserTestBase {
 
   /**
    * Adds a field to the entity_test entity type.
+   *
+   * @param string $field_type
+   *   The field type name (Eg. text).
+   * @param string $widget_type
+   *   The widget name (Eg. text_textfield).
+   * @param array $field_settings
+   *   (optional) An array that gets added to the array passed to
+   *   FieldConfig::create().
+   * @param boolean $comment_alter
+   *   (optional) Option to enable/disable comment_alter for this field.
+   *
+   * @return string
+   *   The name of the field that was created.
    */
-  protected function addField()
-  {
-    // @todo Add field settings to it.
+  protected function addField($field_type, $widget_type, $field_settings = array(), $comment_alter = TRUE) {
     $this->fieldName = Unicode::strtolower($this->randomMachineName() . '_field_name');
 
-    FieldStorageConfig::create(array(
+    FieldStorageConfig::create([
       'field_name' => $this->fieldName,
       'entity_type' => 'entity_test',
-      'type' => 'text'
-    ))->save();
+      'type' => $field_type,
+    ] + $field_settings)->save();
     FieldConfig::create([
       'field_name' => $this->fieldName,
       'entity_type' => 'entity_test',
       'bundle' => 'entity_test_bundle',
+      'widget' => [
+        'type' => $widget_type,
+      ],
+      'third_party_settings' => [
+        'comment_alter' => [
+          'comment_alter_enabled' => $comment_alter,
+        ],
+      ],
     ])->save();
+
     return $this->fieldName;
   }
 
@@ -95,9 +115,10 @@ class CommentAlterTestBase extends BrowserTestBase {
    * @todo Instead of this add other functions here.
    */
   public function testChecking() {
-    $fieldName = $this->addField();
+    $fieldName = $this->addField('text', 'text_textfield');
     $field_storage = FieldStorageConfig::loadByName('entity_test', $fieldName);
-    $this->assertTrue($field_storage, 'Node body field storage exists.');
+    $field = FieldConfig::loadByName('entity_test', 'entity_test_bundle', $fieldName);
+    $this->assertTrue($field_storage, 'Our added field storage exists.');
     $this->assertEqual('1', '1');
   }
 
